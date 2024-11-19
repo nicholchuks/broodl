188757c0610fe38e4ee426c9db6fd25e51d612a2
Nicholas chuks <niccobery91@gmail.com>
2024-11-19 07:35:05 +0100

    first commit

---
+++ app/dashboard/page.js
@@ -0,0 +1,19 @@
+import Dashboard from "@/components/Dashboard";
+import Login from "@/components/Login";
+import Main from "@/components/Main";
+
+export const metadata = {
+  title: "Broodl + Dashboard",
+};
+
+export default function DashboardPage() {
+  const isAuthenticated = false;
+
+  let children = <Login />;
+
+  if (isAuthenticated) {
+    children = ( <Dashboard />)
+  }
+
+  return <Main>{children}</Main>;
+}
